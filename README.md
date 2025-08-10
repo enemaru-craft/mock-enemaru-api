928a826c103bbeb455fcf47861e0a90be9597949
rta_28 <rtasansax@gmail.com>
2025-08-10 20:25:43 +0900

    ➕ add: モックサーバーのコードと起動手順を記載

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mock-enemaru-api
+## 初期設定
+```
+python -m venv .venv
+.venv\Scripts\activate
+pip install -r .\requirements.txt
+```
+## 起動
+```
+.venv\Scripts\activate # まだ仮想環境に入っていない場合に実行
+uvicorn main:app --reload --port 3000
+```
+http://localhost:3000/ でアクセスできる
---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
+isLightEnabled = False
+
+
+class StateModel(BaseModel):
+    isLightEnabled: bool
+    isTrainEnabled: bool = False
+    isFactoryEnabled: bool = False
+    isBlackout: bool = False
+
+
+@app.get("/energy")
+def get_energy():
+    global isLightEnabled
+    return {
+        "state": {
+            "isLightEnabled": isLightEnabled,
+            "isTrainEnabled": False,
+            "isFactoryEnabled": True,
+            "isBlackout": False
+        },
+        "texts": [],
+        "variables": {
+            "totalPowerW": 3200,
+            "surplusPowerW": 450
+        }
+    }
+
+
+@app.post("/state")
+def receive_state(payload: StateModel):
+    print(payload.model_dump_json())
+    global isLightEnabled
+    isLightEnabled = payload.isLightEnabled
+    state = {
+        "isLightEnabled": isLightEnabled,
+        "isTrainEnabled": payload.isTrainEnabled,
+        "isFactoryEnabled": payload.isFactoryEnabled,
+        "isBlackout": payload.isBlackout
+    }
+
+    variables = {
+        "totalPowerW": 3200,
+        "surplusPowerW": 450
+    }
+
+    return {
+        "state": state,
+        "texts": [],
+        "variables": variables
+    }
+
+
+# debug用
+# @app.post("/state")
+# async def receive_state_raw(req: Request):
+#     body = await req.body()
+#     print("RAW:", body.decode("utf-8", errors="replace"))
+#     return {"ok": True}
